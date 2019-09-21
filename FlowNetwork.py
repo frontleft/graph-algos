9a3c4adc9f95349ea202dfb7ac1662f3cf9a13ad
Brayton Holman <brayton.holman@gmail.com>
2019-09-21 16:40:19 -0400

    Complete Ford Fulkerson w/ BFS method for SP

--- FlowNetwork.py
+++ FlowNetwork.py
@@ -5,7 +5,14 @@ Python3 Implementation of flow network algorithms (Ford Fulkerson, etc.)
 
 
 class FlowEdge():
-    def __init__(self, v, w, capacity, flow):
+    def __init__(self, v, w, capacity, flow=0):
+        '''
+        Parameters:
+        v           Int     Index of 'from' vertex
+        w           Int     Index of 'to' vertex
+        capacity    Int     Forward capacity of the edge
+        flow        Int     Amount of forward flow currently going through the edge
+        '''
         self._v = v
         self._w = w
         self._capacity = capacity
@@ -26,6 +33,7 @@ class FlowEdge():
     def w(self):
         return self._w
 
+    @property
     def to_v(self):
         return self._w
 
@@ -52,6 +60,15 @@ class FlowEdge():
             raise ValueError('flow must be >= 0')
 
     def other(self, vertex):
+        '''
+        Returns the index of the other vertex on the edge (i.e. if edge v->w given v returns w)
+
+        Parameters:
+        vertex      Int     Index of a vertex on the edge
+
+        Return:
+        Int                 Index of the other vertex
+        '''
         if (vertex == self._v):
             return self._w
         elif (vertex == self._w):
@@ -60,6 +77,15 @@ class FlowEdge():
             raise IndexError('Invalid endpoint')
 
     def residual_capacity_to(self, vertex):
+        '''
+        Returns the amount of residual capacity (i.e. capacity - flow) to the provided vertex
+
+        Parameters:
+        vertex      Int     Index of a vertex on the edge
+
+        Return:
+        Int                 Amount of flow that can be directed to the vertex
+        '''
         if (vertex == self._v):
             return self._flow
         elif (vertex == self._w):
@@ -68,6 +94,16 @@ class FlowEdge():
             raise IndexError('Invalid endpoint')
 
     def add_resid_flow_to(self, vertex, delta):
+        '''
+        Adds the 'delta' amount of flow along the edge in the direction of the vertex
+
+        Parameters:
+        vertex      Int     Index of a vertex on the edge
+        delta       Int     if > 0, Amount of flow to direct to the vertex, if < 0, amount of flow to remove from the vertex
+
+        Return:
+        void
+        '''
         if (vertex == self._v):
             self.flow -= delta
         elif (vertex == self._w):
@@ -78,6 +114,10 @@ class FlowEdge():
 
 class FlowNetwork():
     def __init__(self, V):
+        '''
+        Parameters:
+        V      Int      Number of vertices within the flow network
+        '''
         self._V = V
         self._E = 0
         self._adj = [[] for v in range(V)]
@@ -103,21 +143,41 @@ class FlowNetwork():
         return self._adj
 
     def add_edge(self, edge):
+        '''
+        Add an edge to the flow network (adds to adj. list of both vertices)
+
+        Parameters:
+        edge        FlowEdge        Flow edge to be added to the network
+
+        Return:
+        void
+        '''
         self.E += 1
         v = edge.from_v
         w = edge.to_v
-        self.adj[v].appened(edge)
+
+        self.adj[v].append(edge)
         self.adj[w].append(edge)
 
 
 class FordFulkerson():
     def __init__(self, G, s, t):
+        '''
+        Parameters:
+        G       FlowNetwork     Flow network to search for augmenting path within
+        s       Int             Source vertex
+        t       Int             Sink vertex  
+        '''
         self._value = 0
         self._marked = [False for v in range(G.V)]
         self.edge_to = [None for v in range(G.V)]
+        self._G = G
+        self._s = s
+        self._t = t
 
         while self.has_augmenting_path(G, s, t):
             bottle = float('inf')
+
             v = t
             while v != s:  # compute bottleneck capacity
                 bottle = min(bottle, self.edge_to[v].residual_capacity_to(v))
@@ -130,23 +190,41 @@ class FordFulkerson():
 
             self._value += bottle
 
+    def __str__(self):
+        s = f'Max flow from {self._s} to {self._t}: \n'
+        for v in range(self._G.V):
+            for edge in self._G.adj[v]:
+                if v == edge.from_v and edge.flow > 0:
+                    s += str(edge) + '\n'
+        s += f'Max flow value = {self._value}\n'
+        return s
+
     def has_augmenting_path(self, G, s, t):
-        edge_to = [None for v in range(G.V)]
-        marked = [False for v in range(G.V)]
+        '''
+        Parameters:
+        G       FlowNetwork     Flow network to search for augmenting path within
+        s       Int             Source vertex
+        t       Int             Sink vertex  
+
+        Return:
+        bool                    True if there is a augmenting path from s->t else false
+        '''
+        self.edge_to = [None for v in range(G.V)]
+        self._marked = [False for v in range(G.V)]
 
         q = deque()
         q.append(s)
-        marked[s] = True
+        self._marked[s] = True
         while len(q) > 0:
             v = q.popleft()
             for edge in G.adj[v]:
                 w = edge.other(v)
-                # while there is a path from s->w
-                if edge.residual_capacity_to(w) > 0 and not marked[w]:
-                    edge_to[w] = edge
-                    marked[w] = True
+                # while there is a path from s->w, save the edge, mark it as known & add to the q
+                if edge.residual_capacity_to(w) > 0 and self._marked[w] == False:
+                    self.edge_to[w] = edge
+                    self._marked[w] = True
                     q.append(w)
-        return marked[t]
+        return self._marked[t]
 
     def in_cut(self, v):
         return self._marked[v]
@@ -154,3 +232,16 @@ class FordFulkerson():
     @property
     def value(self):
         return self._value
+
+
+G = FlowNetwork(6)
+G.add_edge(FlowEdge(0, 1, 2))
+G.add_edge(FlowEdge(0, 2, 3))
+G.add_edge(FlowEdge(1, 3, 3))
+G.add_edge(FlowEdge(1, 4, 1))
+G.add_edge(FlowEdge(2, 3, 1))
+G.add_edge(FlowEdge(2, 4, 1))
+G.add_edge(FlowEdge(3, 5, 2))
+G.add_edge(FlowEdge(4, 5, 3))
+ff_g = FordFulkerson(G, 0, 5)
+print(ff_g)
